c308b9a3866f799244384c7b09237b7d0e663bf5
jminck <jminck@outlook.com>
2018-01-28 16:51:17 -0500

    updates to install.sh

---
+++ swarm-cluster-setup/joinswarm.sh
@@ -0,0 +1,1 @@
+    docker swarm join --token SWMTKN-1-21a9tdngu545wf26ifk27o4lyh3s90vjq8v6fwp47vjgcnai9y-c1g5fv12dsof72k9hdo0is2k0 192.168.1.154:2377
--- swarm-cluster-setup/lib/install.sh
+++ swarm-cluster-setup/lib/install.sh
@@ -6,14 +6,22 @@ sudo service docker start
 sudo chkconfig docker on
 
 #update this to just open needed ports
-echo running firewall-cmd --zone=public --add-port=2377/tcp --permanent
 sudo firewall-cmd --zone=public --add-port=2377/tcp --permanent
-echo running sudo firewall-cmd --reload
-sudo firewall-cmd --reload
+sudo firewall-cmd --permanent --add-port=7946/tcp 
+sudo firewall-cmd --permanent --add-port=4789/udp 
+sudo firewall-cmd --permanent --add-port=7946/udp 
+sudo firewall-cmd --permanent --add-port=2376/tcp 
 
+sudo firewall-cmd --reload
+sudo systemctl restart firewalld
 #initialize swarm on master node
 if [ $(hostname -s) == "swarmnode1" ]
 then
-    sudo docker swarm init --advertise-addr $ip > ~/joinswarm.txt
+    sudo docker swarm init --advertise-addr $(ifconfig enp0s8 | grep mask | awk '{print $2}'| cut -f2 -d:) | grep SWMTKN > /vagrant/joinswarm.sh
 fi
 
+#install workers
+if [ $(hostname -s) != "swarmnode1" ]
+then
+    sudo /vagrant/joinswarm.sh
+fi
