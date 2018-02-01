fdaadb0397d77e2b7b773094cf1ac984081a3f32
Joseph Minckler <jminck@outlook.com>
2018-02-01 00:49:10 -0600

    install.sh updates

--- swarm-cluster-setup/lib/install.sh
+++ swarm-cluster-setup/lib/install.sh
@@ -14,6 +14,12 @@ sudo firewall-cmd --permanent --add-port=2376/tcp
 
 sudo firewall-cmd --reload
 sudo systemctl restart firewalld
+sudo service docker restart
+
+#install docker compose
+sudo curl -L https://github.com/docker/compose/releases/download/1.18.0/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+
 #initialize swarm on master node
 if [ $(hostname -s) == "swarmnode1" ]
 then
@@ -23,5 +29,8 @@ fi
 #install workers
 if [ $(hostname -s) != "swarmnode1" ]
 then
-    sudo /vagrant/joinswarm.sh
+    CMDLINE=$(</vagrant/joinswarm.sh)
+    TOKEN="$(cut -d' ' -f5 <<<$CMDLINE)"
+    MGR="$(cut -d' ' -f6 <<<$CMDLINE)"
+    docker swarm join --token $TOKEN --advertise-addr $(ifconfig enp0s8 | grep mask | awk '{print $2}'| cut -f2 -d:) $MGR
 fi
