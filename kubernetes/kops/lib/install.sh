a84f88264876825c7d5d30fac8b6d02ad3678a93
Joseph Minckler <jminck@outlook.com>
2018-02-07 15:07:15 -0600

    kubernetes stuff

--- kubernetes/kops/lib/install.sh
+++ kubernetes/kops/lib/install.sh
@@ -5,17 +5,8 @@ chmod +x kops-linux-amd64
 mv kops-linux-amd64 /usr/local/bin/kops
 
 #get python pip
-sudo apt-get install python-pip 
+sudo apt-get install python-pip -y -q
 sudo pip install awscli
 
-#update this to just open needed ports
-sudo firewall-cmd --zone=public --add-port=2377/tcp --permanent
-sudo firewall-cmd --permanent --add-port=7946/tcp 
-sudo firewall-cmd --permanent --add-port=4789/udp 
-sudo firewall-cmd --permanent --add-port=7946/udp 
-sudo firewall-cmd --permanent --add-port=2376/tcp 
 
-sudo firewall-cmd --reload
-sudo systemctl restart firewalld
-sudo service docker restart
 
